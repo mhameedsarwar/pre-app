22d09b8b660324fdf3b941fca662460aee67ea7f
Muhammad Hameed Sarwar <112hameed@gmail.com>
2024-08-25 16:11:59 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hi React</h1>
+        <p>My name is Muhammad Hameed</p>
+      </div>
+    </>
+  )
+}
+
+export default App
